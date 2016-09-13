f7070729477ac47c5181bc0866dca3ccf7d63774
Dmytro Rybin <dmytro_rybin@epam.com>
2016-09-13 15:55:07 +0300

    @Before added

--- src/test/java/ua/epam/ArithmeticOperationsTest.java
+++ src/test/java/ua/epam/ArithmeticOperationsTest.java
@@ -1,6 +1,7 @@
 package ua.epam;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,30 +10,33 @@ import static org.junit.Assert.*;
  * Created by Dmytro_Rybin on 9/13/2016.
  */
 public class ArithmeticOperationsTest {
+    public ArithmeticOperations ao;
+
+    @Before
+    public void runT() {
+        ao = new ArithmeticOperations();
+    }
+
     @Test
     public void add() throws Exception {
-        ArithmeticOperations ao = new ArithmeticOperations();
         double res = ao.add(5, 7);
         Assert.assertTrue(res == 12);
     }
 
     @Test
     public void deduct() throws Exception {
-        ArithmeticOperations ao = new ArithmeticOperations();
         double res = ao.deduct(7, 5);
         Assert.assertEquals(res, 2, 0);
     }
 
     @Test
     public void mult() throws Exception {
-        ArithmeticOperations ao = new ArithmeticOperations();
         double res = ao.mult(5, 3);
         if(res != 15) Assert.fail();
     }
 
     @Test
     public void dev() throws Exception {
-        ArithmeticOperations ao = new ArithmeticOperations();
         double res = ao.dev(14, 7);
         if(res != 2) Assert.fail();
     }
