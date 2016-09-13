e84b9207d15a485d536eb96ec490ff70e62875ca
Dmytro Rybin <dmytro_rybin@epam.com>
2016-09-13 15:29:33 +0300

    unit tests added

---
+++ src/test/java/ua/epam/ArithmeticOperationsTest.java
@@ -0,0 +1,40 @@
+package ua.epam;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dmytro_Rybin on 9/13/2016.
+ */
+public class ArithmeticOperationsTest {
+    @Test
+    public void add() throws Exception {
+        ArithmeticOperations ao = new ArithmeticOperations();
+        double res = ao.add(5, 7);
+        if(res != 12) Assert.fail();
+    }
+
+    @Test
+    public void deduct() throws Exception {
+        ArithmeticOperations ao = new ArithmeticOperations();
+        double res = ao.deduct(7, 5);
+        if(res != 2) Assert.fail();
+    }
+
+    @Test
+    public void mult() throws Exception {
+        ArithmeticOperations ao = new ArithmeticOperations();
+        double res = ao.mult(5, 3);
+        if(res != 15) Assert.fail();
+    }
+
+    @Test
+    public void dev() throws Exception {
+        ArithmeticOperations ao = new ArithmeticOperations();
+        double res = ao.dev(14, 7);
+        if(res != 2) Assert.fail();
+    }
+
+}
