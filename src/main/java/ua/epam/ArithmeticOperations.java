a2d4e70a61a3154c77b129ca6bff71c8e460eda0
Dmytro Rybin <dmytro_rybin@epam.com>
2016-09-13 15:10:56 +0300

    first commit

---
+++ src/main/java/ua/epam/ArithmeticOperations.java
@@ -0,0 +1,24 @@
+package ua.epam;
+
+/**
+ * Created by Dmytro_Rybin on 9/13/2016.
+ */
+public class ArithmeticOperations {
+
+    public double add(double value1, double value2) {
+        return value1 + value2;
+    }
+
+    public double deduct(double value1, double value2) {
+        return value1 - value2;
+    }
+
+    public double mult(double value1, double value2) {
+        return value1 * value2;
+    }
+
+    public double dev(double value1, double value2) {
+        return value1 / value2;
+    }
+
+}
